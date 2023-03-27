54d41e6c9d5f6878e8df0fc98aae330729e102f2
PraiseAbieyuwa <alohanpraise2002@gmail.com>
2023-03-27 09:19:06 -0700

    precision

--- precision.c
+++ precision.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * get_precision - calculates the precision for printing
- * @format: Formatted strings in which to print the argu
- * @i: list of argu to be printed
- * @list: list of all argu
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
- * Return: return precision
+ * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_1 + *i + i;
+	int curr_i = *i + 1;
 	int precision = -1;
 
 	if (format[curr_i] != '.')
